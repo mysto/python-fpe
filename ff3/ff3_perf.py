3fef70bc28d13e9958ca19658518f284eaac1023
Brad Schoening <5796692+bschoening@users.noreply.github.com>
2021-10-09 22:52:19 -0400

    updated to issolate just the encrypt function for timing

--- ff3/ff3_perf.py
+++ ff3/ff3_perf.py
@@ -11,23 +11,26 @@ def timeit(f):
         result = f(*args, **kw)
         te = time.time()
 
-        print( 'func:%r args:[%r, %r] took: %2.4f sec' % \
-          (f.__name__, args, kw, te-ts))
+        print(f'func:{f.__name__} took: {te-ts:2.4f}')
         return result
 
     return timed
 
 @timeit
-def test_performance(runs=100000):
+def test_encrypt(plaintext):
 
         key = "EF4359D8D580AA4F7F036D6F04FC6A94"
         tweak = "D8E7920AFA330A73"
+        for txt in plaintext:
+            c = FF3Cipher(key, tweak, 62)
+            s = c.encrypt(txt)
+
+def test_performance(runs=100_000):
+
         plaintext = []
         for i in range(runs):
             plaintext.append(''.join(random.choices(string.ascii_uppercase + string.digits, k=8)))
-        for i in range(runs):
-            c = FF3Cipher(key, tweak, 62)
-            s = c.encrypt(plaintext[i])
+        test_encrypt(plaintext)
 
 if __name__ == '__main__':
    test_performance()
